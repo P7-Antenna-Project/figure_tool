cc2967e016096d408a984c8baac346bc80a699aa
Apbk20 <73229358+Apbk20@users.noreply.github.com>
2023-10-16 14:19:43 +0200

    Forsøg på oprettelse af collection

---
+++ figures/01Introduction.py
@@ -0,0 +1,19 @@
+from figurelib.figure import FigureCollection
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Some example data to display
+
+collection = FigureCollection("01_Introduction")
+
+
+@collection.table(header=["Col 1", "Col 2", "Col 3"], header_spec="l|l|l")
+def test_table():
+    header = ["Col 1", "Col 2", "Col 3"]
+    data = []
+
+    for _ in range(10):
+        data_row = {col: np.random.randint(0, 99) for col in header}
+        data.append(data_row)
+
+    return data
--- figures/index.py
+++ figures/index.py
@@ -1,10 +1,12 @@
 from figurelib.figure import FigureContainer
 
 from . import collection1
+from . import 01Introduction
 
 
 main_container = FigureContainer(
     [
         collection1.collection,
+        01Introduction.collection,
     ]
 )
