dd962694622e8f96995567912abc80b8da2b2160
Apbk20 <73229358+Apbk20@users.noreply.github.com>
2023-11-28 09:18:02 +0100

    Comparison of models

--- figures/index.py
+++ figures/index.py
@@ -3,12 +3,12 @@ from figurelib.figure import FigureContainer
 from . import Introduction1
 from . import appendix_Antenna_design
 from . import Technical03
+from . import appendix_Simple_model
 main_container = FigureContainer(
     [
         Introduction1.collection,
         Technical03.collection,
         appendix_Antenna_design.collection,
-
-    
+        appendix_Simple_model.collection,
     ]
 )
